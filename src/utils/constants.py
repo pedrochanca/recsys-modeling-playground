deab6a634c7df11bb863a7dd0e971dfb13c32aae
Pedro C. <pedrom.mchanca@gmail.com>
2025-12-19 23:48:28 +0000

    add constants

---
+++ src/utils/constants.py
@@ -0,0 +1,9 @@
+# Default column names
+DEFAULT_USER_COL = "user_id"
+DEFAULT_ITEM_COL = "item_id"
+DEFAULT_TARGET_COL = "target"
+DEFAULT_TIMESTAMP_COL = "timestamp"
+DEFAULT_PREDICTION_COL = "prediction"
+
+# Other
+SEED = 42
